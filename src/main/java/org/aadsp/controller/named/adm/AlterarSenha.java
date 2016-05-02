4cd8fb18a6cae246d2b98f00340cf38140301850
Felipe Coelho <felipe.pereira.coelho@gmail.com>
2016-05-01 21:29:26 -0300

    Inclusão de página de alteração de senha do usuário do sistema

---
+++ src/main/java/org/aadsp/controller/named/adm/AlterarSenha.java
@@ -0,0 +1,60 @@
+
+package org.aadsp.controller.named.adm;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import org.aadsp.annotations.Usuario;
+import org.aadsp.interfaces.ABaseNamed;
+import org.aadsp.utils.Mensageiro;
+import static org.aadsp.utils.Mensageiro.mensagemInfo;
+import org.aadsp.utils.Session;
+
+/**
+ * Classe que representa o objeto de tela Pessoal Consulta
+ * @author Felipe Coelho
+ * @version  25/04/2016
+ */
+@ViewScoped
+@Named
+public class AlterarSenha extends ABaseNamed
+{   
+    public void alterar()
+    {
+      try
+      {
+        if(senha.equals(RepetirSenha))
+        {
+           Usuario usuario = (Usuario) Session.getAttribute("usuario");
+           usuario.setSenha(senha);
+           usuario.editar();
+           Mensageiro.mensagemInfo("Senha alterada com sucesso");
+        }
+        else
+        {
+            Mensageiro.mensagemWarn("Você não repetiu a senha como solicitado!");
+        }
+      }catch(Exception e)
+      {
+          Mensageiro.mensagemError("Não foi possível alterar a senha!");
+      }
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getRepetirSenha() {
+        return RepetirSenha;
+    }
+
+    public void setRepetirSenha(String RepetirSenha) {
+        this.RepetirSenha = RepetirSenha;
+    }
+    
+    private String senha;
+    private String RepetirSenha;
+}
