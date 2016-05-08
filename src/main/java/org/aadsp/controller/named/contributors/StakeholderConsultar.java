73baa16ac771e2989e42d80c789a3eaebeddaae0
Felipe Coelho <felipe.pereira.coelho@gmail.com>
2016-05-08 18:33:10 -0300

    Modificações em funções em gerências - foi modificado para registro de empresas e stakeholder para colaboradores, visualização de dados de usuários em adm e modificação das estrutura de reports cada um deve estar em seu proprio diretorio

---
+++ src/main/java/org/aadsp/controller/named/contributors/StakeholderConsultar.java
@@ -0,0 +1,51 @@
+
+package org.aadsp.controller.named.contributors;
+
+import java.util.List;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import org.aadsp.annotations.Stakeholder;
+import org.aadsp.interfaces.ABaseNamed;
+import org.aadsp.utils.Criptografia;
+import org.aadsp.utils.Mensageiro;
+import org.aadsp.utils.Response;
+
+/**
+ * Classe que representa o objeto de tela Pessoal Consulta
+ * @author Felipe Coelho
+ * @version  02/05/2016
+ */
+@ViewScoped
+@Named
+public class StakeholderConsultar extends ABaseNamed
+{
+    
+   public StakeholderConsultar()
+   {
+       this.stakeholder = new Stakeholder();
+   }
+   
+   public List<Stakeholder> getListarStakeholders()
+   {
+       try{
+       return this.stakeholder.listar();
+       }catch(Exception e){
+           Mensageiro.mensagemError("Erro ao listar stakeholders cadastrados!!");
+       }
+       return null;
+   }
+   
+   public void editar(Stakeholder stakeholder)
+   {
+       try
+       {
+        Response.redirect("/web/faces/views/projetos/StakeholderEditar.xhtml?Stakeholder="+ Criptografia.codificarParaBase64(stakeholder.getID().toString()));
+       }catch(Exception e)
+       {
+         Mensageiro.mensagemError("Erro ao selecionar Página!!");
+       }
+   }
+   
+   private Stakeholder stakeholder;
+    
+}
--- src/main/java/org/aadsp/utils/RelatorioIReport.java
+++ src/main/java/org/aadsp/utils/RelatorioIReport.java
@@ -20,11 +20,17 @@ import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 public class RelatorioIReport 
 {
+    /**
+     * Metódo para gerar report em pdf - é criado um report em pdf no servidor na pasta tmp, não apagar este diretório, o reltatório deverá
+     * ser criado em um diretório de mesmo nome que seu principal.
+     * @param nomeRelatorio deve ser informado o nome correspondente ao relatório principal
+     * @param parameters deverá ser passado os paramentros em chave e seu valor correpondente ao report
+     */
     public void gerarPDF(String nomeRelatorio,Map parameters) throws JRException, IOException, SQLException
     {
          String caminhoServidor = FacesContext.getCurrentInstance().getExternalContext().getRealPath("");
        
-         InputStream inputStream = new FileInputStream (caminhoServidor+"/reports/model/"+nomeRelatorio+".jrxml");
+         InputStream inputStream = new FileInputStream (caminhoServidor+"/reports/model/"+nomeRelatorio+"/"+nomeRelatorio+".jrxml");
          
          JasperDesign jasperDesign = JRXmlLoader.load(inputStream);
          JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
