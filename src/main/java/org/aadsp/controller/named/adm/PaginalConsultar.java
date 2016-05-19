208d557569015d4903336c291f47faa644d4c71a
Felipe Coelho <felipe.pereira.coelho@gmail.com>
2016-05-19 17:21:18 -0300

    Construção do Filtro de pesquisa com clausula de AND e OR Classe filtro

--- src/main/java/org/aadsp/controller/named/adm/PaginalConsultar.java
+++ src/main/java/org/aadsp/controller/named/adm/PaginalConsultar.java
@@ -1,51 +1,69 @@
-
 package org.aadsp.controller.named.adm;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import org.aadsp.annotations.Pagina;
 import org.aadsp.interfaces.ABaseNamed;
 import org.aadsp.utils.Criptografia;
+import org.aadsp.utils.Filtro;
 import org.aadsp.utils.Mensageiro;
 import org.aadsp.utils.Response;
 
 /**
  * Classe que representa o objeto de tela Pessoal Consulta
+ *
  * @author Felipe Coelho
- * @version  24/04/2016
+ * @version 24/04/2016
  */
 @ViewScoped
 @Named
-public class PaginalConsultar extends ABaseNamed
-{
+public class PaginalConsultar extends ABaseNamed {
+
+    public PaginalConsultar() {
+        this.pagina = new Pagina();
+        this.filtro = new Filtro();
+        criarFiltro();
+    }
     
-   public PaginalConsultar()
-   {
-       this.pagina = new Pagina();
-   }
-   
-   public List<Pagina> getListarPaginas()
-   {
-       try{
-       return this.pagina.listar();
-       }catch(Exception e){
-           Mensageiro.mensagemError("Erro ao listar páginas!!");
-       }
-       return null;
-   }
-   
-   public void editar(Pagina pagina)
-   {
-       try
-       {
-        Response.redirect("/web/faces/views/adm/PaginaEditar.xhtml?Pagina="+ Criptografia.codificarParaBase64(pagina.getID().toString()));
-       }catch(Exception e)
-       {
-         Mensageiro.mensagemError("Erro ao selecionar Página!!");
-       }
-   }
-   
-   private Pagina pagina;
+    private void criarFiltro()
+    {
+        Map<String,String> atributo = new HashMap<>();
+        Map<String,String> atributoClasse = new HashMap<>();
+        
+        atributo.put("Nome", "nome");
+        atributoClasse.put("nome", "Pagina");
+        filtro.setAtributo(atributo,atributoClasse);
+    }
+
+    public List<Pagina> getListarPaginas() {
+        try {
+            return this.pagina.listar();
+        } catch (Exception e) {
+            Mensageiro.mensagemError("Erro ao listar páginas!!");
+        }
+        return null;
+    }
+
+    public void editar(Pagina pagina) {
+        try {
+            Response.redirect("/web/faces/views/adm/PaginaEditar.xhtml?Pagina=" + Criptografia.codificarParaBase64(pagina.getID().toString()));
+        } catch (Exception e) {
+            Mensageiro.mensagemError("Erro ao selecionar Página!!");
+        }
+    }
+
+    public Filtro getFiltro() {
+        return filtro;
+    }
+
+    public void setFiltro(Filtro filtro) {
+        this.filtro = filtro;
+    }
     
+    private Pagina pagina;
+    private Filtro filtro;
+
 }
---
+++ src/main/java/org/aadsp/utils/Filtro.java
@@ -0,0 +1,181 @@
+package org.aadsp.utils;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Filtro {
+
+    public Filtro() {
+        addOperadorCondicional();
+        addOperadorLogico();
+        this.atributo = new HashMap<>();
+        this.atributoClasse = new HashMap<>();
+    }
+
+    private void addOperadorCondicional() {
+        this.operadorCondicional = new HashMap<>();
+        operadorCondicional.put("E", "AND");
+        operadorCondicional.put("OU", "OR");
+    }
+
+    private void addOperadorLogico() {
+        this.operadorLogico = new HashMap<>();
+        operadorLogico.put("IGUAL", "=");
+        operadorLogico.put("DIFERENTE", "<>");
+        operadorLogico.put("MAIOR OU IGUAL", ">=");
+        operadorLogico.put("MENOR OU IGUAL", "<=");
+        operadorLogico.put("SEMELHANTE", "LIKE");
+        operadorLogico.put("MAIOR", ">");
+        operadorLogico.put("MENOR", "<");
+    }
+
+    public void onAtributoSelecionado() throws ClassNotFoundException {
+        addOperadorLogico();
+        if (atributoSelecionado != null) {
+            String nomeClasseAtributo = atributoClasse.get(atributoSelecionado);
+            String tipoAtributo = "";
+            for (Field atributoClasse : Class.forName("org.aadsp.annotations." + nomeClasseAtributo).getDeclaredFields()) {
+                if (atributoClasse.getName().equals(atributoSelecionado)) {
+                    tipoAtributo = atributoClasse.getType().getName();
+
+                }
+            }
+
+            tipoAtributo = tipoAtributo.replace("java.lang.", "");
+
+            if (tipoAtributo.equals("String")) {
+                operadorLogico.remove("MAIOR OU IGUAL");
+                operadorLogico.remove("MENOR OU IGUAL");
+                operadorLogico.remove("MAIOR");
+                operadorLogico.remove("MENOR");
+            }else {
+                operadorLogico.remove("SEMELHANTE");
+            }
+
+            if (filtro == null) {
+                filtro = "( " + atributoSelecionado + " ";
+            } else if (!filtro.endsWith(")") && !atributoSelecionado.equals("FILTRAR POR")) {
+                filtro = filtro + " ";
+            }else if(!filtro.endsWith(") ") && !atributoSelecionado.equals("FILTRAR POR"))
+            {
+                filtro = filtro + " " + operadorCondicionalSelecionada + " ( " + atributoSelecionado + " ";
+            }
+        }else
+        {
+            operadorLogico.remove("IGUAL");
+            operadorLogico.remove("DIFERENTE");
+            operadorLogico.remove("MAIOR OU IGUAL");
+            operadorLogico.remove("MENOR OU IGUAL");
+            operadorLogico.remove("SEMELHANTE");
+            operadorLogico.remove("MAIOR");
+            operadorLogico.remove("MENOR");
+        }
+
+    }
+
+    public void onOperadorLogicoSelecionado() {
+        if (operadorLogicoSelecionado != null) {
+            if (filtro != null) {
+                if (filtro.endsWith(" ")) {
+                        filtro = filtro + operadorLogicoSelecionado;
+                }
+            }
+        }
+    }
+
+    public Map<String, String> getOperadorCondicional() {
+        return operadorCondicional;
+    }
+
+    public void setOperadorCondicional(Map<String, String> operadorCondicional) {
+        this.operadorCondicional = operadorCondicional;
+    }
+
+    public String getOperadorCondicionalSelecionada() {
+        return operadorCondicionalSelecionada;
+    }
+
+    public void setOperadorCondicionalSelecionada(String operadorCondicionalSelecionada) {
+        this.operadorCondicionalSelecionada = operadorCondicionalSelecionada;
+    }
+
+    public Map<String, String> getAtributo() {
+        return atributo;
+    }
+
+    public void setAtributo(Map<String, String> atributo, Map<String, String> atributoClasse) {
+        this.atributo = atributo;
+        this.atributoClasse = atributoClasse;
+    }
+
+    public String getAtributoSelecionado() {
+        return atributoSelecionado;
+    }
+
+    public void setAtributoSelecionado(String atributoSelecionado) {
+        this.atributoSelecionado = atributoSelecionado;
+    }
+
+    public Map<String, String> getOperadorLogico() {
+        return operadorLogico;
+    }
+
+    public void setOperadorLogico(Map<String, String> operadorLogico) {
+        this.operadorLogico = operadorLogico;
+    }
+
+    public String getOperadorLogicoSelecionado() {
+        return operadorLogicoSelecionado;
+    }
+
+    public void setOperadorLogicoSelecionado(String operadorLogicoSelecionado) {
+        this.operadorLogicoSelecionado = operadorLogicoSelecionado;
+    }
+
+    public String getAtributoFiltro() {
+        return atributoFiltro;
+    }
+
+    public void setAtributoFiltro(String atributoFiltro) {
+        this.atributoFiltro = atributoFiltro;
+    }
+
+    public boolean getOnAtributoSelecionado() {
+        return atributoSelecionado != null;
+    }
+
+    public void filtrar() {
+        if (operadorLogicoSelecionado != null) {
+            if (filtro != null) {
+                if (filtro.endsWith(" ")) {
+                        filtro = filtro +" "+atributoFiltro+")";
+                }
+            }
+        }
+    }
+
+    public String getFiltro() {
+        return filtro;
+    }
+
+    public void setFiltro(String filtro) {
+        this.filtro = filtro;
+    }
+
+    public Map<String, String> operadorCondicional;
+    public String operadorCondicionalSelecionada;
+    public Map<String, String> atributo;
+    public Map<String, String> atributoClasse;
+    public String atributoSelecionado;
+    public Map<String, String> operadorLogico;
+    public String operadorLogicoSelecionado;
+    public String atributoFiltro;
+    public String filtro;
+
+}
