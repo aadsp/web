1d1a804dbf1c6465122ee416d3cc8ad3c535c96e
Felipe Coelho <felipe.pereira.coelho@gmail.com>
2016-05-22 23:26:06 -0300

    Inclusão de filtro em Reuniões do Projeto, correção de erro em exibição

--- src/main/java/org/aadsp/controller/named/project/ReunioesConsultar.java
+++ src/main/java/org/aadsp/controller/named/project/ReunioesConsultar.java
@@ -1,59 +1,87 @@
-
 package org.aadsp.controller.named.project;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import net.sf.jasperreports.engine.JRException;
 import org.aadsp.annotations.Projeto;
 import org.aadsp.interfaces.ABaseNamed;
 import org.aadsp.utils.Criptografia;
+import org.aadsp.utils.Filtro;
 import org.aadsp.utils.Mensageiro;
 import org.aadsp.utils.Response;
 
 /**
  * Classe que representa o objeto de tela Pessoal Consulta
+ *
  * @author Felipe Coelho
- * @version  24/04/2016
+ * @version 24/04/2016
  */
 @ViewScoped
 @Named
-public class ReunioesConsultar extends ABaseNamed
-{
+public class ReunioesConsultar extends ABaseNamed {
+
+    public ReunioesConsultar() {
+        try {
+            this.projeto = new Projeto();
+            this.listaProjetos = this.projeto.listar();
+            this.filtro = new Filtro();
+            criarFiltro();
+        } catch (Exception e) {
+            Mensageiro.mensagemError("Erro ao listar os projetos!!");
+        }
+    }
     
-   public ReunioesConsultar()
-   {
-       this.projeto = new Projeto();
-   }
-   
-   public List<Projeto> listarProjetos()
-   {
-       try{
-       return this.projeto.listar();
-       }catch(Exception e){
-           Mensageiro.mensagemError("Erro ao listar os projetos!!");
-       }
-       return null;
-   }
-   
-   public void editar(Projeto projeto) 
-   {
-       try
-       {
-        Response.redirect("/web/faces/views/projetos/ATAConsultar.xhtml?Projeto="+ Criptografia.codificarParaBase64(projeto.getID().toString()));
-       }catch(Exception e)
-       {
-         Mensageiro.mensagemError("Erro ao selecionar Projeto!!");
-       }
-   }
-   
-   public void gerarPDF(Projeto projeto) throws JRException, IOException, SQLException
-   {
-      
-   }
-   
-   private Projeto projeto;
+    private void criarFiltro() {
+        Map<String, String> atributo = new HashMap<>();
+        Map<String, String> atributoClasse = new HashMap<>();
+
+        atributo.put("Nome", "t.nome");
+        atributoClasse.put("t.nome", "TAP");
+        filtro.setAtributo(atributo, atributoClasse);
+    }
     
+    public List<Projeto> listarProjetos() {
+        return this.listaProjetos;
+    }
+
+    public void editar(Projeto projeto) {
+        try {
+            Response.redirect("/web/faces/views/projetos/ATAConsultar.xhtml?Projeto=" + Criptografia.codificarParaBase64(projeto.getID().toString()));
+        } catch (Exception e) {
+            Mensageiro.mensagemError("Erro ao selecionar Projeto!!");
+        }
+    }
+
+    public void gerarPDF(Projeto projeto) throws JRException, IOException, SQLException {
+
+    }
+    
+    public void filtroConsulta() {
+        try {
+            if (this.filtro.filtro.endsWith(")")) {
+                listaProjetos = this.projeto.listarPorFiltro(filtro.filtro);
+            }else
+                listaProjetos = this.projeto.listar();
+        } catch (Exception e) {
+            Mensageiro.mensagemError("Não foi possível consultar pelo filtro gerado!!");
+        }
+    }
+
+    public Filtro getFiltro() {
+        return filtro;
+    }
+
+    public void setFiltro(Filtro filtro) {
+        this.filtro = filtro;
+    }
+
+    private Projeto projeto;
+    private List<Projeto> listaProjetos;
+    private Filtro filtro;
+
 }
